d59616689acb29cf92e464b0504d63d8991ecbc6
Baran Orek <52622731+BaranOrek@users.noreply.github.com>
2019-07-25 08:37:38 +0300

    updated , expAnimation failed

--- app/src/main/java/com/example/balonn/MainActivity.java
+++ app/src/main/java/com/example/balonn/MainActivity.java
@@ -14,21 +14,23 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 import com.google.ar.sceneform.Camera;
 import com.google.ar.sceneform.Node;
 import com.google.ar.sceneform.Scene;
+import com.google.ar.sceneform.animation.ModelAnimator;
 import com.google.ar.sceneform.collision.Ray;
 import com.google.ar.sceneform.math.Vector3;
+import com.google.ar.sceneform.rendering.AnimationData;
 import com.google.ar.sceneform.rendering.MaterialFactory;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.rendering.ShapeFactory;
 import com.google.ar.sceneform.rendering.Texture;
+import com.google.ar.sceneform.ux.BaseArFragment;
 
-import java.net.URI;
-import java.util.Arrays;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -40,18 +42,25 @@ public class MainActivity extends AppCompatActivity {
     private Camera camera;
     private ModelRenderable bulletRenderable;
     private ModelRenderable tntRenderable;
+    //private Renderable expRenderable;
     private boolean shouldStartTimer = true;
-    private int balloonsLeft = 20;
+    private  int balloonsLeft = 20;
+    private int minutesPassed;
+    private int secondsPassed;
     boolean countdownBoolean = false;
     private Point point;
     private TextView balloonsLeftTxt;
     private TextView countdown;
     private SoundPool soundPool;
+    public Node contactIter;
+    //public Node nodeinAct2;
     int timeLeft;
     private int sound;
     Button bt_restart;
     ArrayList<Vector3> balloons = new ArrayList<>();
     ArrayList<Vector3> tnts = new ArrayList<>();
+    //ArrayList<Vector3> exps = new ArrayList<>();
+    //private ModelRenderable expRenderable;
 
 
     @Override
@@ -70,6 +79,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 countdown.setVisibility(View.INVISIBLE);
                 countdownBoolean = true;
+                startTimer();
+                Log.i("aaa", String.valueOf(countdownBoolean));
             }
         });
 
@@ -127,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         countdownBoolean = false;
     }
 
-    private void shoot() {
+    public void shoot() {
         Ray ray = camera.screenPointToRay(point.x / 2f, point.y / 2f);
         Node node = new Node();
         node.setRenderable(bulletRenderable);
@@ -152,14 +163,16 @@ public class MainActivity extends AppCompatActivity {
 
                         if (nodeInContact.getName().equals("TNT")) {
 
-                            final Handler handler = new Handler();
+                            tntExplode(nodeInContact , node);
+
+                            /*final Handler handler = new Handler();
                             Runnable tntExplosion = new Runnable() {
-                                int tntX = (int) nodeInContact.getLocalPosition().x - 5;
-                                int xBound = tntX + 10;
-                                int tntY = (int) (nodeInContact.getLocalPosition().y * 10f) - 5;
-                                int yBound = tntY + 10;
-                                int tntZ = (int) nodeInContact.getLocalPosition().z - 5;
-                                int zBound = tntZ + 10;
+                                int tntX = (int) nodeInContact.getLocalPosition().x - 3;
+                                int xBound = tntX + 6;
+                                int tntY = (int) (nodeInContact.getLocalPosition().y * 10f) - 3;
+                                int yBound = tntY + 6;
+                                int tntZ = (int) nodeInContact.getLocalPosition().z - 3;
+                                int zBound = tntZ + 6;
                                 @Override
                                 public void run() {
 
@@ -197,20 +210,25 @@ public class MainActivity extends AppCompatActivity {
                                                         soundPool.play(sound, 1f, 1f, 1, 0
                                                                 , 1f);
 
+                                                    }else if (nodeInExp.getName().equals("TNT")){
+
+
+
                                                     }
+
                                                 }
 
                                             }
                                         }
                                     }
 
-                                    handler.postDelayed(this, 10000);
+                                    handler.postDelayed(this, 100);
 
                                 }
 
 
                             };
-                            tntExplosion.run();
+                            tntExplosion.run();*/
                             //Log.d("D","Exploded");
                             //double min1=0;
                             // double min2=0;
@@ -286,11 +304,14 @@ public class MainActivity extends AppCompatActivity {
 
 
                             if (balloonsLeft == 0) {
-                                countDownMode();
-                                addBaloonsToScene();
-                                balloonsLeft = 20;
-
+                                /*countDownMode();
                                 startTimer();
+                                addBaloonsToScene();
+                                balloonsLeft = 20;*/
+                                Intent restartIntent = getBaseContext().getPackageManager()
+                                        .getLaunchIntentForPackage(getBaseContext().getPackageName());
+                                restartIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                startActivity(restartIntent);
 
                             }
 
@@ -321,7 +342,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public double getDistance(Vector3 v1, Vector3 v2) {
+    /*public double getDistance(Vector3 v1, Vector3 v2) {
         double i;
         i = Math.sqrt(((v1.x - v2.x) * (v1.x - v2.x) + ((v1.y - v2.y) * (v1.y - v2.y)) + ((v1.z - v2.z) * (v1.z - v2.z))));
         return i;
@@ -336,7 +357,7 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
         return false;
-    }
+    }*/
 
 
     @Override
@@ -372,27 +393,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startTimer() {
-        TextView timer = findViewById(R.id.timerText);
-        new Thread(() -> {
 
-            int seconds = 0;
-            while (balloonsLeft > 0) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        TextView timer = findViewById(R.id.timerText);
+
+        if(countdownBoolean) {
+
+            new Thread(() -> {
+
+                int seconds = 0;
+                while (balloonsLeft != 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+
+                    }
+                    seconds++;
+
+                    minutesPassed = seconds / 60;
+                    secondsPassed = seconds % 60;
+                    Log.i("aaa" , "bbb");
+
+                    runOnUiThread(() -> timer.setText(minutesPassed + ":" + secondsPassed));
+
 
                 }
-                seconds++;
 
-                int minutesPassed = seconds / 60;
-                int secondsPassed = seconds % 60;
+            }).start();
 
-                runOnUiThread(() -> timer.setText(minutesPassed + ":" + secondsPassed));
-
-            }
-
-        }).start();
+        }
 
 
     }
@@ -444,7 +473,7 @@ public class MainActivity extends AppCompatActivity {
                 .setSource(this, Uri.parse("model.sfb"))
                 .build()
                 .thenAccept(renderable -> {
-                    for (int i = 0; i < 4; i++) {
+                    for (int i = 0; i < 15; i++) {
                         Node node = new Node();
                         node.setRenderable(renderable);
                         scene.addChild(node);
@@ -465,6 +494,92 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    /*private void addExp() {
+
+        // Create the ModelRenderable.
+        //final Object[] andyRenderable = new Object[1];
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("explosion.sfb"))
+                .build()
+                .thenAccept(renderable -> expRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Log.e("aaa", "Unable to load Renderable.", throwable);
+                            return null;
+                        });
+
+        // Attach the ModelRenderable to the node in the scene.
+        Node andyNode = new Node();
+
+        andyNode.setRenderable(expRenderable);
+        for (int i = 0; i < 4; i++) {
+            //AnimationData expAnimate = expRenderable;
+            Node node = new Node();
+            node.setRenderable(expRenderable);
+            scene.addChild(node);
+            node.setName("exp");
+            AnimationData expAnimation = expRenderable.getAnimationData("exp");
+            ModelAnimator expAnimator = new ModelAnimator(expAnimation , expRenderable);
+            expAnimator.start();
+
+            //Log.i("aaa" , expAnimation.getName());
+
+            Random random = new Random();
+            int x = random.nextInt(10);
+            int y = random.nextInt(10);
+            int z = random.nextInt(15);
+
+            z = -z;
+            Vector3 v1 = new Vector3((float) x,
+                    y / 10f,
+                    (float) z);
+            exps.add(v1);
+            node.setWorldPosition(v1);
+        }
+
+
+
+        // Create the ModelRenderable.
+        ModelRenderable.builder()
+                .setSource(this, Uri.parse("explosion.sfb"))
+                .build()
+                .thenAccept(renderable -> {
+
+                    // Attach the ModelRenderable to the node in the scene.
+                    for (int i = 0; i < 4; i++) {
+                        //AnimationData expAnimate = expRenderable;
+                        Node node = new Node();
+                        node.setRenderable(renderable);
+                        scene.addChild(node);
+                        node.setName("exp");
+                        AnimationData expAnimation = renderable.getAnimationData("exp");
+                        ModelAnimator expAnimator = new ModelAnimator(expAnimation , renderable);
+                        expAnimator.start();
+
+                        //Log.i("aaa" , expAnimation.getName());
+
+                        Random random = new Random();
+                        int x = random.nextInt(10);
+                        int y = random.nextInt(10);
+                        int z = random.nextInt(15);
+
+                        z = -z;
+                        Vector3 v1 = new Vector3((float) x,
+                                y / 10f,
+                                (float) z);
+                        exps.add(v1);
+                        node.setWorldPosition(v1);
+                    }
+
+
+                });
+    }*/
+
+
+
+
+
 
 
     private void addBaloonsToScene() {
@@ -495,4 +610,235 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    public void tntExplode(Node contact , Node pubNode) {
+
+
+            int tntX = (int) contact.getLocalPosition().x - 3;
+            int xBound = tntX + 6;
+            int tntY = (int) (contact.getLocalPosition().y * 10f) - 3;
+            int yBound = tntY + 6;
+            int tntZ = (int) contact.getLocalPosition().z - 3;
+            int zBound = tntZ + 6;
+
+
+                for (int axisX = tntX ; axisX < xBound; axisX++) {
+
+                    for (int axisY = tntY; axisY < yBound; axisY++) {
+
+                        for (int axisZ = tntZ ; axisZ < zBound ; axisZ++) {
+
+                            Vector3 vExpArea = new Vector3((float)axisX/1f , axisY/10f , (float)axisZ/1f);
+                            pubNode.setWorldPosition(vExpArea);
+                            Node nodeInExp = scene.overlapTest(pubNode);
+                            if (nodeInExp != null) {
+
+                                if (nodeInExp.getName().equals("Balloon")) {
+
+                                    balloonsLeft--;
+                                    balloons.remove(vExpArea);
+                                    // Log.d("D","Ballooonnn");
+                                    balloonsLeftTxt.setText("Balloons Left: " + balloonsLeft);
+
+                                    scene.removeChild(nodeInExp);
+
+
+                                    if (balloonsLeft == 0) {
+                                        /*countDownMode();
+                                        startTimer();
+                                        addBaloonsToScene();
+                                        balloonsLeft = 20;*/
+
+                                            Intent restartIntent = getBaseContext().getPackageManager()
+                                                    .getLaunchIntentForPackage(getBaseContext().getPackageName());
+                                            restartIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                            startActivity(restartIntent);
+
+
+
+
+                                    }
+
+
+                                    soundPool.play(sound, 1f, 1f, 1, 0
+                                            , 1f);
+
+                                }else if (nodeInExp.getName().equals("TNT")){
+
+                                    tntExplodeIter(nodeInExp);
+
+
+                                    /*tntLeft--;
+                                    tnts.remove(vExpArea);
+                                    // Log.d("D","Ballooonnn");
+
+                                    scene.removeChild(nodeInExp);
+
+
+                                    if (tntLeft == 0) {
+                                        addTnt();
+                                        tntLeft = 4;
+                                    }*/
+                                }
+                            }
+                        }
+                    }
+                }
+    }
+
+                //*****************************************************************************************//
+
+    public void tntExplodeIter(Node contactIter) {
+
+
+        int tntX = (int) contactIter.getLocalPosition().x - 3;
+        int xBound = tntX + 6;
+        int tntY = (int) (contactIter.getLocalPosition().y * 10f) - 3;
+        int yBound = tntY + 6;
+        int tntZ = (int) contactIter.getLocalPosition().z - 3;
+        int zBound = tntZ + 6;
+
+
+        for (int axisX = tntX ; axisX < xBound; axisX++) {
+
+            for (int axisY = tntY; axisY < yBound; axisY++) {
+
+                for (int axisZ = tntZ ; axisZ < zBound ; axisZ++) {
+
+                    Vector3 vExpArea = new Vector3((float)axisX/1f , axisY/10f , (float)axisZ/1f);
+                    contactIter.setWorldPosition(vExpArea);
+                    Node nodeInExp1 = scene.overlapTest(contactIter);
+                    if (nodeInExp1 != null) {
+
+                        if (nodeInExp1.getName().equals("Balloon")) {
+
+                            balloonsLeft--;
+                            balloons.remove(vExpArea);
+                            // Log.d("D","Ballooonnn");
+                            balloonsLeftTxt.setText("Balloons Left: " + balloonsLeft);
+
+                            scene.removeChild(nodeInExp1);
+
+
+                            if (balloonsLeft == 0) {
+                                /*countDownMode();
+                                startTimer();
+                                addBaloonsToScene();
+                                balloonsLeft = 20;*/
+
+                                    Intent restartIntent = getBaseContext().getPackageManager()
+                                            .getLaunchIntentForPackage(getBaseContext().getPackageName());
+                                    restartIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(restartIntent);
+
+
+
+
+                            }
+
+
+                            soundPool.play(sound, 1f, 1f, 1, 0
+                                    , 1f);
+
+                        }else if (nodeInExp1.getName().equals("TNT")){
+
+
+                            tntLeft--;
+                            tnts.remove(vExpArea);
+                            // Log.d("D","Ballooonnn");
+
+                            scene.removeChild(nodeInExp1);
+
+
+                            if (tntLeft == 0) {
+                                addTnt();
+                                tntLeft = 4;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+        /*final Handler handler = new Handler();
+        Runnable tntExplosion = new Runnable() {
+
+            public void run() {
+
+                for (int radius = 0 ; radius < 4 ; radius++) {
+
+                    for (int angleBeta = 0 ; angleBeta < 180; angleBeta++) {
+
+                        for (int angleTeta = 0 ; angleTeta < 360 ; angleTeta++) {
+
+                            Double angle1 = new Double(angleBeta);
+                            Double angle2 = new Double(angleTeta);
+                            double axisX = radius * Math.cos(angle1) * Math.sin(angle2);
+                            double axisY = radius * Math.sin(angle1) * Math.sin(angle2);
+                            double axisZ = radius * Math.cos(angle2);
+
+
+                            Vector3 vExpArea = new Vector3(contact.getLocalPosition().x + (float)axisX/1f , contact.getLocalPosition().y+(float)axisY/10f ,
+                                    contact.getLocalPosition().z + (float)axisZ/1f);
+                            pubNode.setWorldPosition(vExpArea);
+                            Node nodeInExp = scene.overlapTest(pubNode);
+                            if (nodeInExp != null) {
+
+                                if (nodeInExp.getName().equals("Balloon")) {
+
+                                    balloonsLeft--;
+                                    balloons.remove(vExpArea);
+                                    // Log.d("D","Ballooonnn");
+                                    balloonsLeftTxt.setText("Balloons Left: " + balloonsLeft);
+
+                                    scene.removeChild(nodeInExp);
+
+
+                                    if (balloonsLeft == 0) {
+                                        countDownMode();
+                                        addBaloonsToScene();
+                                        balloonsLeft = 20;
+
+                                        startTimer();
+
+                                    }
+
+
+                                    soundPool.play(sound, 1f, 1f, 1, 0
+                                            , 1f);
+
+                                }//else if (nodeInExp.getName().equals("TNT")){
+
+                                    //tntExplosion();
+
+                                //}
+
+                            }
+
+                        }
+                    }
+                }
+
+                handler.postDelayed(this, 0);
+
+            }
+
+
+        };
+        tntExplosion.run();*/
+
+
+
+
 }
